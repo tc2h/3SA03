b370c5c060611362cf3e178a19bd5a3f114138f2
tc2h <47141265+tc2h@users.noreply.github.com>
2019-09-27 21:13:09 +0700

    CHANG SIZE AND COLOR

--- WordCard.js
+++ WordCard.js
@@ -34,6 +34,7 @@ export default class WordCard extends Component {
             <div className="App">	            
             <div className="App">	
 
+                <p>Hint : The first program in C </p>
                 {
                 Array.from(this.state.chars).map((c, i) => <CharacterCard value = {c} key = {i} 
                 attempt={this.state.attempt}
@@ -41,9 +42,9 @@ export default class WordCard extends Component {
                 )
                 }
                <p>Round : {this.state.attempt}</p>
-                <p>{this.state.completed? "you win !!" : ""}</p>
                <p>Hint : The first program in C </p>
                 <p>{this.state.completed? "You Win !!" : ""}</p>
+                <h1><font color="green">{this.state.completed? "You Win !!" : ""}</font></h1>
                 </div>	           
             );	       
 
